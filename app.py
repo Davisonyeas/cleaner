5433384e67d2043e02af8d880ba3b44047be2129
davisonyeas <davisonyeas1@gmail.com>
2022-09-03 09:18:10 +0100

    added cv

--- app.py
+++ app.py
@@ -257,7 +257,7 @@ def main():
         # Path 
         current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
         css_file = current_dir / "styles" / "main.css"
-        resume_file = current_dir / "assets" / "Davis'_ resume.pdf"
+        resume_file = current_dir / "assets" / "Davis'_CV.pdf"
         profile_pic = current_dir / "assets" / "Davis_pic_tr_best.png"
 
 
